b1614618904725072fe311db3787b12a0ab1ce6c
jessicaw9494 <jessicaw9494@hstat.org>
2016-10-12 17:21:35 +0000

    finish lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,26 @@
-def division
-    
+def division(x=42, y=7)
+    x/y
 end
 
-def argue
-  
+def argue(phrase)
+    "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(parameter="cheese")
+    parameter
 end
+pizza_party
 
-def whisper
-    
+
+def whisper(parameter="Hey!")
+    parameter.downcase
 end
+whisper
